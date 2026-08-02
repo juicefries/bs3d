24752dd80d1189156e7d8c11830171e75c81e8a1
juicefries <drinkFruitJuiceOfFrenchFries@outlook.com>
2026-08-03 04:32:16 +0800

    0.0.1

---
+++ src/main/java/io/github/juicefries/bs3d/Start.java
@@ -0,0 +1,14 @@
+//
+// Created by juicefries
+// The project name is bs3d
+// Data 2026/08/03 04:19
+//
+
+package io.github.juicefries.bs3d;
+
+// As you can see, I still don't know certain things.
+public class Start {
+    static void main() {
+        IO.println("Block Survival 3D");
+    }
+}
